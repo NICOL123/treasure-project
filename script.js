d5033ae003facb804fb25968cc575459a1921375
NICOL123 <43378862+NICOL123@users.noreply.github.com>
2019-02-07 09:11:28 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+ $(".img").click(function() {
+  $(".butt1").show();
+});
+ $(".img").click(function() {
+  $(".img").hide();
+});
+
+ $(".butt1").click(function() {
+  $("body").css("background-color","black");
+});
+ $(".h1").click(function() {
+  $(".h1").text("!!! money money money $$$$$ !!!");
+});
+
+
